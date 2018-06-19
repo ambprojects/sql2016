764d75cceeb8700292adac1ad3e1e1308b4b9d0e
ambprojects <alpasbra_@hotmail.com>
2018-06-18 20:49:46 -0500

    descriptopn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'LDR';
+  private description = 'Bienvenido a la página de LDR'
 }
